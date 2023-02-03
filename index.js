b9cd3f172a3f359727696f3b43ca1e0451d19d7b
Tasfiajahan <tania.develpers@gmail.com>
2023-02-03 19:46:15 +0600

    server update

--- index.js
+++ index.js
@@ -250,5 +250,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`easy-transport-server listening on port... ${port}`);
+  console.log(`easy-transport-server listening on port.... ${port}`);
 });
