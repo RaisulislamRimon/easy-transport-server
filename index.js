01fc1737fdad335fc907d3ae90205d4783a9be38
Tasfiajahan <tania.develpers@gmail.com>
2023-02-03 19:29:45 +0600

    server update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xsvxy9i.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-ejywpg7-shard-00-00.xsvxy9i.mongodb.net:27017,ac-ejywpg7-shard-00-01.xsvxy9i.mongodb.net:27017,ac-ejywpg7-shard-00-02.xsvxy9i.mongodb.net:27017/?ssl=true&replicaSet=atlas-b37q9i-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xsvxy9i.mongodb.net/?retryWrites=true&w=majority&ssl=true`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -39,6 +40,9 @@ async function run() {
   try {
     const database = client.db("easy-transport");
     const transportsCollection = database.collection("transports");
+    const divisions = database.collection("divisions");
+    const districts = database.collection("districts");
+    const bookings = database.collection("bookings");
     // const reviewsCollection = database.collection("reviews");
     const usersCollection = database.collection("users");
 
@@ -53,6 +57,26 @@ async function run() {
       };
       const result = await usersCollection.insertOne(query);
       console.log(result);
+      res.send(result);
+    });
+
+    app.get("/division", async (req, res) => {
+      const query = {};
+      const cursor = divisions.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/store/booking", async (req, res) => {
+      const data = req.body;
+      const result = await bookings.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/booking/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const cursor = bookings.find(query);
+      const result = await cursor.toArray();
       res.send(result);
     });
 
